efb4f33f2da1622227d97c9a76d34cb908769813
Anusha-17 <98573933+Anusha-17@users.noreply.github.com>
2023-12-19 11:30:33 +0530

    XML Parsing program

--- Thread/thread.cpp
+++ Thread/thread.cpp
@@ -2,24 +2,41 @@
 #include<stdlib.h>
 #include<pthread.h>
 #define COUNT 4
-void* threadfunction(void* arg)
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *threadfunction(void *arg)
 {
-	char* data = (char*)arg;
+	char *data = (char*)arg;
+	 pthread_mutex_lock( &mutex );
 	system(data);// /mnt/bin/camera/qcarcamtest/qcarcamcam_test -config=* data
+	pthread_mutex_unlock( &mutex );
 	pthread_exit(NULL);
 }
-const char* mycaminfo[COUNT][70]= {
-			"/mnt/bin/camera/qcarcamtest/12cam.xml",
-			"/mnt/bin/camera/qcarcamtest/1cam.xml",
-			"/mnt/bin/camera/qcarcamtest/8cam.xml",
-			"/mnt/bin/camera/qcarcamtest/cam_id9.xml",
-}; 
+/*
+const char* mycaminfo[COUNT]= {
+			"/mnt/bin/camera/qcarcam_test/qcarcam_test -config=/mnt/bin/camera/qcarcam_test/12cam.xml",
+			"/mnt/bin/camera/qcarcam_test/qcarcam_test -config=/mnt/bin/camera/qcarcam_test/1cam.xml",
+			"/mnt/bin/camera/qcarcam_test/qcarcam_test -config=/mnt/bin/camera/qcarcam_test/8cam.xml",
+			"/mnt/bin/camera/qcarcam_test/qcarcam_test -config=/mnt/bin/camera/qcarcam_test/cam_id9.xml",
+};
+*/
+
+const char mycaminfo[COUNT][120]= {
+                         //"audio_chime -g 21 /var/LRMonoPhase4"
+			"/mnt/bin/camera/qcarcam_test/qcarcam_test -config=/mnt/bin/camera/qcarcam_test/cam_id9.xml",
+			"/mnt/bin/camera/qcarcam_test/qcarcam_test -config=/mnt/bin/camera/qcarcam_test/1cam.xml",
+			"/mnt/bin/camera/qcarcam_test/qcarcam_test -config=/mnt/bin/camera/qcarcam_test/8cam.xml",
+			"/mnt/bin/camera/qcarcam_test/qcarcam_test -config=/mnt/bin/camera/qcarcam_test/12cam.xml",
+};
+ 
 int main()
 {
 	pthread_t threads[COUNT];
 	for(int i=0;i<COUNT;i++)
 	{
-		int result = pthread_create(&threads[i], NULL, threadfunction, (void*)mycaminfo[i]);
+	        //printf("%s\n",mycaminfo[i]);
+		int result = pthread_create(&threads[i], NULL, &threadfunction, (void*)&mycaminfo[i]);
 		if(result)
 		{
 			printf("Error creating thread %d\n", i);
